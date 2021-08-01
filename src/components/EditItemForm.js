fd6914a8b74f3cb1d64ccc8ea586d519ef68f231
dansagam <olukayodeogunnowo@yahoo.com>
2021-08-01 21:39:38 +0100

    add the components edit form to the application

---
+++ src/components/EditItemForm.js
@@ -0,0 +1,25 @@
+
+import React, {useState} from 'react'
+import { Input, Button, Form } from 'reactstrap'
+import {  useDispatch, useSelector } from 'react-redux'
+import { selectItem,  } from '../reducers/ItemReducers'
+
+const EditItemForm = (props) => {
+   // eslint-disable-next-line no-unused-vars
+   const itemValue = useSelector(selectItem)
+   const dispatch = useDispatch();
+
+   const [name, setName] = useState(props.editValue)
+   const onSubmit = (e) => {
+      e.preventDefault()
+      dispatch('')
+   }
+   return (
+      <Form onSubmit={onSubmit} >
+         <Input type='text' name={name} value={name} onChange={(e) => setName(e.target.value)} />
+         <Button color='dark' style={{marginTop: '2rem'}}>Save Item</Button>
+      </Form>
+   )
+}
+
+export default EditItemForm
