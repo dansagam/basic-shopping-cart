bbef4aed6241d8eec1b7cfeb86829053122bba16
dansagam <olukayodeogunnowo@yahoo.com>
2021-08-19 11:38:06 +0100

    added a return keyword to the res.status

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,7 +9,7 @@ function auth(req, res, next){
    const token = req.header('x-auth-token')
 
    if(!token){
-      res.status(401).json({msg: "unauthorised token"})
+      return res.status(401).json({msg: "unauthorised token"})
    }
    try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
