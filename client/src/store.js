b2e26fc851ee137e4167f16f47fdb042486fed42
dansagam <olukayodeogunnowo@yahoo.com>
2021-08-23 11:31:56 +0100

    modified the state store

--- client/src/store.js
+++ client/src/store.js
@@ -1,25 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
-import rootReducer from './reducers/ItemReducers'
+import  AuthReducers  from './reducers/authReducers'
+import  ErrorReducers  from './reducers/errorReducers'
+import ItemReducers from './reducers/ItemReducers'
 
 
-// const initialState = {
-// ///dsdksdsdksdkds
-// }
-// const middleware = [thunk];
 export default configureStore({
    reducer: {
-      item: rootReducer,
-   },
+      item: ItemReducers,
+      auth: AuthReducers,
+      error: ErrorReducers
+   }
 })
-
-// const store = createStore(
-//    rootReducer, 
-//    initialState, 
-//    compose(
-//       applyMiddleware(...middleware),
-//       window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//    )
-// )
-
-
-// export default store;
