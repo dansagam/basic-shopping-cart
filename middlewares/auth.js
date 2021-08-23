78fef9c378f9b23577ce303d192753f8331353a6
dansagam <olukayodeogunnowo@yahoo.com>
2021-08-23 11:30:32 +0100

    ad the middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,17 +6,22 @@ import { config } from 'dotenv'
       config({path: '../config/config.env'})
 
 function auth(req, res, next){
+   console.log('testing me')
    const token = req.header('x-auth-token')
 
    if(!token){
+      console.log('loo')
       return res.status(401).json({msg: "unauthorised token"})
    }
    try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      console.log(decoded)
       req.user = decoded
       next()
    } catch (error) {
-      res.status(400).json({msg: 'token not valid'})
+      console.log('fight')
+      console.log(error)
+      return res.status(401).json({msg: 'token not valid'})
    }
 }
 
