37b0a2885995dc174f2c319be97dd0d572d0e6f5
dansagam <olukayodeogunnowo@yahoo.com>
2021-08-25 04:16:17 +0100

    component update of the loaduser action creator

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
 import  AuthReducers  from './reducers/authReducers'
-import  ErrorReducers  from './reducers/errorReducers'
 import ItemReducers from './reducers/ItemReducers'
 
 
@@ -8,6 +7,5 @@ export default configureStore({
    reducer: {
       item: ItemReducers,
       auth: AuthReducers,
-      error: ErrorReducers
    }
 })
